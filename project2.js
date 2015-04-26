2fbe92b00992035bd07a5b17f5f375304c4910fb
hadgiske <hadgiske@clarkson.edu>
2015-04-26 17:12:59 -0400

    Update project2.js

--- project2.js
+++ project2.js
@@ -1,43 +1,17 @@
-var CharY = 0;
-var CharZ = 0;
-var CharX = 0;
+var CharY = 0, CharZ = 0, CharX = 0, CharL = 0, CharR = 0, CharT = 0, CharB = 0;
+var Sqr1X = 15, Sqr1Y = getRandomInt() * 8, Sqr1XL = 0, Sqr1XR = 0, Sqr1XT = 0, Sqr1XB = 0;
+var Sqr2X = 8, Sqr2Y = getRandomInt() * 8, Sqr2XL = 0, Sqr2XR = 0, Sqr2XT = 0, Sqr2XB = 0;
+var Sqr3X = getRandomInt() * 8, Sqr3Y = 15, Sqr3XL = 0, Sqr3XR = 0, Sqr3XT = 0, Sqr3XB = 0;
+var Sqr4X = getRandomInt() * 8, Sqr4Y = 8, Sqr4XL = 0, Sqr4XR = 0, Sqr4XT = 0, Sqr4XB = 0;
+var Sqr5X = getRandomInt() * 8, Sqr5Y = getRandomInt() * 8, Sqr5Z = -8, Sqr5XL = 0, Sqr5XR = 0, Sqr5XT = 0, Sqr5XB = 0, Sqr5XN = 0, Sqr5XF = 0;
+var Sqr6X = getRandomInt() * 8, Sqr6Y = getRandomInt() * 8, Sqr6Z = -15, Sqr6XL = 0, Sqr6XR = 0, Sqr6XT = 0, Sqr6XB = 0, Sqr6XN = 0, Sqr6XF = 0;
 
-var CharL = 0;
-var CharR = 0;
-var CharT = 0;
-var CharB = 0;
-
-var Sqr1X = 8;
-var Sqr1Y = 0;
-
-var Sqr1XL = 0;
-var Sqr1XR = 0;
-var Sqr1XT = 0;
-var Sqr1XB = 0;
-
-var Sqr2X = 6;
-var Sqr2Y = 0;
-
-var Sqr2XL = 0;
-var Sqr2XR = 0;
-var Sqr2XT = 0;
-var Sqr2XB = 0;
-
-var Sqr3X = 0;
-var Sqr3Y = 6;
-
-var Sqr3XL = 0;
-var Sqr3XR = 0;
-var Sqr3XT = 0;
-var Sqr3XB = 0;
-
-var Sqr4X = 0;
-var Sqr4Y = 6;
-
-var Sqr4XL = 0;
-var Sqr4XR = 0;
-var Sqr4XT = 0;
-var Sqr4XB = 0;
+var timer = 0;
+var play = true;
+var texSize = 1;
+var texCoordsArray = [];
+var texture;
+var texCoord = [0,0,0,1,1,1,1,0];
 
 // Shadow.js (c) 2012 matsuda and tanaka
 // Vertex shader program for generating a shadow map
@@ -79,6 +53,7 @@ var FSHADER_SOURCE =
   'uniform sampler2D u_ShadowMap;\n' +
   'varying vec4 v_PositionFromLight;\n' +
   'varying vec4 v_Color;\n' +
+  'uniform sampler2D newTexture;\n' + 
   'void main() {\n' +
   '  vec3 shadowCoord = (v_PositionFromLight.xyz/v_PositionFromLight.w)/2.0 + 0.5;\n' +
   '  vec4 rgbaDepth = texture2D(u_ShadowMap, shadowCoord.xy);\n' +
@@ -88,7 +63,7 @@ var FSHADER_SOURCE =
   '}\n';
 
 var OFFSCREEN_WIDTH = 2048, OFFSCREEN_HEIGHT = 2048;
-var LIGHT_X = 0, LIGHT_Y = 7, LIGHT_Z = 2; // Position of the light source
+var LIGHT_X = 0, LIGHT_Y = 3, LIGHT_Z = -.25; // Position of the light source
 
 window.onload = function main() {
   // Retrieve <canvas> element
@@ -130,6 +105,8 @@ window.onload = function main() {
   var square2 = initVertexBuffersForSquareOtherX(gl);
   var square3 = initVertexBuffersForSquareY(gl);
   var square4 = initVertexBuffersForSquareOtherY(gl);
+  var square5 = initVertexBuffersForSquare5(gl);
+  var square6 = initVertexBuffersForSquare6(gl);
   if (!triangle || !plane) {
     console.log('Failed to set the vertex information');
     return;
@@ -150,7 +127,7 @@ window.onload = function main() {
 
   var viewProjMatrixFromLight = new Matrix4(); // Prepare a view projection matrix for generating a shadow map
   viewProjMatrixFromLight.setPerspective(70.0, OFFSCREEN_WIDTH/OFFSCREEN_HEIGHT, 1.0, 100.0);
-  viewProjMatrixFromLight.lookAt(LIGHT_X, LIGHT_Y, LIGHT_Z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  viewProjMatrixFromLight.lookAt(LIGHT_X, LIGHT_Y, LIGHT_Z, 0, 0, 0, 0.0, 1.0, 0.0);
 
   var viewProjMatrix = new Matrix4();          // Prepare a view projection matrix for regular drawing
   viewProjMatrix.setPerspective(60, canvas.width/canvas.height, 1.0, 100.0);
@@ -167,6 +144,8 @@ window.onload = function main() {
 	square2 = initVertexBuffersForSquareOtherX(gl);
 	square3 = initVertexBuffersForSquareY(gl);
     square4 = initVertexBuffersForSquareOtherY(gl);
+	square5 = initVertexBuffersForSquare5(gl);
+	square6 = initVertexBuffersForSquare6(gl);
 	document.onkeydown = handleKeyDown;
   	document.onkeyup = handleKeyUp;
 	
@@ -174,6 +153,8 @@ window.onload = function main() {
 	squareTwo();
 	squareThree();
 	squareFour();
+	squareFive();
+	squareSix();
 	
 	winLossCheck();
 
@@ -183,17 +164,21 @@ window.onload = function main() {
 
     gl.useProgram(shadowProgram); // Set shaders for generating a shadow map
     // Draw the triangle and the plane (for generating a shadow map)
-    drawTriangle(gl, shadowProgram, triangle, currentAngle, viewProjMatrixFromLight);
+    draw(gl, shadowProgram, triangle, viewProjMatrix);
     mvpMatrixFromLight_t.set(g_mvpMatrix); // Used later
-	drawSquare(gl, shadowProgram, square1, currentAngle, viewProjMatrixFromLight);
+	draw(gl, shadowProgram, square1, viewProjMatrix);
     mvpMatrixFromLight_t.set(g_mvpMatrix); // Used later
-	drawSquareOtherX(gl, shadowProgram, square2, currentAngle, viewProjMatrixFromLight);
+	draw(gl, shadowProgram, square2, viewProjMatrix);
     mvpMatrixFromLight_t.set(g_mvpMatrix); // Used later
-	drawSquareY(gl, shadowProgram, square3, currentAngle, viewProjMatrixFromLight);
+	draw(gl, shadowProgram, square3, viewProjMatrix);
     mvpMatrixFromLight_t.set(g_mvpMatrix); // Used later
-	drawSquareOtherY(gl, shadowProgram, square4, currentAngle, viewProjMatrixFromLight);
+	draw(gl, shadowProgram, square4, viewProjMatrix);
     mvpMatrixFromLight_t.set(g_mvpMatrix); // Used later
-    drawPlane(gl, shadowProgram, plane, viewProjMatrixFromLight);
+	draw(gl, shadowProgram, square5, viewProjMatrix);
+    mvpMatrixFromLight_t.set(g_mvpMatrix); // Used later
+	draw(gl, shadowProgram, square6, viewProjMatrix);
+    mvpMatrixFromLight_t.set(g_mvpMatrix); // Used later
+	draw(gl, shadowProgram, plane, viewProjMatrix);
     mvpMatrixFromLight_p.set(g_mvpMatrix); // Used later
 
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);               // Change the drawing destination to color buffer
@@ -201,64 +186,52 @@ window.onload = function main() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);    // Clear color and depth buffer
 
     gl.useProgram(normalProgram); // Set the shader for regular drawing
-    gl.uniform1i(normalProgram.u_ShadowMap, 0);  // Pass 0 because gl.TEXTURE0 is enabledする
+    gl.uniform1i(normalProgram.u_ShadowMap, 0);  // Pass 0 because gl.TEXTURE0 is enabled
     // Draw the triangle and plane ( for regular drawing)
     gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_t.elements);
-    drawTriangle(gl, normalProgram, triangle, currentAngle, viewProjMatrix);
+	draw(gl, normalProgram, triangle, viewProjMatrix);
 	gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_t.elements);
-    drawSquare(gl, normalProgram, square1, currentAngle, viewProjMatrix);
+	draw(gl, normalProgram, square1, viewProjMatrix);
 	gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_t.elements);
-    drawSquareOtherX(gl, normalProgram, square2, currentAngle, viewProjMatrix);
+	draw(gl, normalProgram, square2, viewProjMatrix);
 	gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_t.elements);
-    drawSquareY(gl, normalProgram, square3, currentAngle, viewProjMatrix);
+	draw(gl, normalProgram, square3, viewProjMatrix);
+	gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight,  false, mvpMatrixFromLight_t.elements);
+	draw(gl, normalProgram, square4, viewProjMatrix);
 	gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_t.elements);
-    drawSquareOtherY(gl, normalProgram, square4, currentAngle, viewProjMatrix);
+	draw(gl, normalProgram, square5, viewProjMatrix);
+	gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_t.elements);
+	draw(gl, normalProgram, square6, viewProjMatrix);
     gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_p.elements);
-    drawPlane(gl, normalProgram, plane, viewProjMatrix);
-
+	draw(gl, normalProgram, plane, viewProjMatrix);
+	
     window.requestAnimationFrame(tick, canvas);
   };
-  tick(); 
+	tick();
 }
+function configureTexture( image ) {
+    texture = gl.createTexture();
+    gl.bindTexture( gl.TEXTURE_2D, texture );
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, 
+         gl.RGB, gl.UNSIGNED_BYTE, image );
+    gl.generateMipmap( gl.TEXTURE_2D );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, 
+                      gl.NEAREST_MIPMAP_LINEAR );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+    
+    gl.uniform1i(gl.getUniformLocation(normalProgram, "newTexture"), 0);
+}
+setInterval(function(){ 
+	if(play == true){
+		timer += 5; 
+		document.getElementById("display").innerHTML = "Score: " + timer;
+	}
+}, 500); // 5pts per half second
 
 // Coordinate transformation matrix
 var g_modelMatrix = new Matrix4();
 var g_mvpMatrix = new Matrix4();
-function drawTriangle(gl, program, triangle, angle, viewProjMatrix) {
-  // Set rotate angle to model matrix and draw triangle
-  g_modelMatrix.setRotate(angle, 0, 1, 0);
-  draw(gl, program, triangle, viewProjMatrix);
-}
-
-function drawSquare(gl, program, square1, angle, viewProjMatrix) {
-  // Set rotate angle to model matrix and draw triangle
-  g_modelMatrix.setRotate(angle, 0, 1, 0);
-  draw(gl, program, square1, viewProjMatrix);
-}
-
-function drawSquareOtherX(gl, program, square1, angle, viewProjMatrix) {
-  // Set rotate angle to model matrix and draw triangle
-  g_modelMatrix.setRotate(angle, 0, 1, 0);
-  draw(gl, program, square1, viewProjMatrix);
-}
-
-function drawSquareY(gl, program, square3, angle, viewProjMatrix) {
-  // Set rotate angle to model matrix and draw triangle
-  g_modelMatrix.setRotate(angle, 0, 1, 0);
-  draw(gl, program, square3, viewProjMatrix);
-}
-
-function drawSquareOtherY(gl, program, square4, angle, viewProjMatrix) {
-  // Set rotate angle to model matrix and draw triangle
-  g_modelMatrix.setRotate(angle, 0, 1, 0);
-  draw(gl, program, square4, viewProjMatrix);
-}
-
-function drawPlane(gl, program, plane, viewProjMatrix) {
-  // Set rotate angle to model matrix and draw plane
-  g_modelMatrix.setRotate(-45, 0, 1, 1);
-  draw(gl, program, plane, viewProjMatrix);
-}
 
 function draw(gl, program, o, viewProjMatrix) {
   initAttributeVariable(gl, program.a_Position, o.vertexBuffer);
@@ -292,7 +265,7 @@ function initVertexBuffersForPlane(gl) {
 
   // Vertex coordinates
   var vertices = new Float32Array([
-    15.0, -5.7, -2.5,  -1.0, -9.7, 9.5,  -15.0, -15.7, 5.0,   8.0, 0.6, -10.5    // v0-v1-v2-v3
+    8, -7, 0,  -8, -7, 0,  -8, -7, -7,   8, -7, -7    // v0-v1-v2-v3
   ]);
 
   // Colors
@@ -572,6 +545,134 @@ function initVertexBuffersForSquareOtherY(gl) {
   return o;
 }
 
+function initVertexBuffersForSquare5(gl) {
+  // Create a plane
+  //  v1------v0
+  //  |        | 
+  //  |        |
+  //  |        |
+  //  v2------v3
+
+  // Vertex coordinates
+  var vertices = new Float32Array([
+    1.5 + Sqr5X, 0.5 + Sqr5Y,  0.25 + Sqr5Z,
+    1.5 + Sqr5X, 0.5 + Sqr5Y, -0.25 + Sqr5Z,
+    1.5 + Sqr5X, 1.0 + Sqr5Y,  0.25 + Sqr5Z,
+	1.5 + Sqr5X, 1.0 + Sqr5Y, -0.25 + Sqr5Z,
+    1.0 + Sqr5X,  0.5 + Sqr5Y,  0.25 + Sqr5Z,
+    1.0 + Sqr5X,  0.5 + Sqr5Y, -0.25 + Sqr5Z,
+    1.0 + Sqr5X,  1.0 + Sqr5Y,  0.25 + Sqr5Z,
+	1.0 + Sqr5X,  1.0 + Sqr5Y, -0.25 + Sqr5Z
+  ]);
+  
+  Sqr5XL = 1.0 + Sqr5X;
+  Sqr5XR = 1.5 + Sqr5X;
+  Sqr5XT = 1.0 + Sqr5Y;
+  Sqr5XB = 0.5 + Sqr5Y;
+  Sqr5XN = 0.25 + Sqr5Z;
+  Sqr5XF = -0.25 + Sqr5Z;
+
+  // Colors
+  var colors = new Float32Array([
+    0.0, 0.6, 0.0,  0.0, 0.0, 0.0,   0.0, 0.0, 0.0,   0.0, 0.0, 0.0,
+	0.0, 0.6, 0.0,    0.0, 0.0, 0.0,    0.0, 0.0, 0.0,   0.0, 0.0, 0.0
+  ]);
+
+  // Indices of the vertices
+  var indices = new Uint8Array([
+  	1, 3, 7,
+	7, 5, 1,
+    2, 3, 1, 
+    1, 0, 2, 
+    7, 3, 2, 
+    6, 7, 2, 
+    5, 7, 6, 
+    6, 4, 5, 
+	6, 2, 0, 
+	0, 4, 6, 
+	5, 1, 0, 
+	0, 4, 5 ]);
+
+  var o = new Object(); // Utilize Object object to return multiple buffer objects together
+
+  // Write vertex information to buffer object
+  o.vertexBuffer = initArrayBufferForLaterUse(gl, vertices, 3, gl.FLOAT);
+  o.colorBuffer = initArrayBufferForLaterUse(gl, colors, 3, gl.FLOAT);
+  o.indexBuffer = initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE);
+  if (!o.vertexBuffer || !o.colorBuffer || !o.indexBuffer) return null; 
+
+  o.numIndices = indices.length;
+
+  // Unbind the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  return o;
+}
+function initVertexBuffersForSquare6(gl) {
+  // Create a plane
+  //  v1------v0
+  //  |        | 
+  //  |        |
+  //  |        |
+  //  v2------v3
+
+  // Vertex coordinates
+  var vertices = new Float32Array([
+    1.5 + Sqr6X, 0.5 + Sqr6Y,  0.25 + Sqr6Z,
+    1.5 + Sqr6X, 0.5 + Sqr6Y, -0.25 + Sqr6Z,
+    1.5 + Sqr6X, 1.0 + Sqr6Y,  0.25 + Sqr6Z,
+	1.5 + Sqr6X, 1.0 + Sqr6Y, -0.25 + Sqr6Z,
+    1.0 + Sqr6X,  0.5 + Sqr6Y,  0.25 + Sqr6Z,
+    1.0 + Sqr6X,  0.5 + Sqr6Y, -0.25 + Sqr6Z,
+    1.0 + Sqr6X,  1.0 + Sqr6Y,  0.25 + Sqr6Z,
+	1.0 + Sqr6X,  1.0 + Sqr6Y, -0.25 + Sqr6Z
+  ]);
+  
+  Sqr6XL = 1.0 + Sqr6X;
+  Sqr6XR = 1.5 + Sqr6X;
+  Sqr6XT = 1.0 + Sqr6Y;
+  Sqr6XB = 0.5 + Sqr6Y;
+  Sqr6XN = 0.25 + Sqr6Z;
+  Sqr6XF = -0.25 + Sqr6Z;
+
+  // Colors
+  var colors = new Float32Array([
+    0.0, 0.6, 0.0,  0.0, 0.0, 0.0,   0.0, 0.0, 0.0,   0.0, 0.0, 0.0,
+	0.0, 0.6, 0.0,    0.0, 0.0, 0.0,    0.0, 0.0, 0.0,   0.0, 0.0, 0.0
+  ]);
+
+  // Indices of the vertices
+  var indices = new Uint8Array([
+  	1, 3, 7,
+	7, 5, 1,
+    2, 3, 1, 
+    1, 0, 2, 
+    7, 3, 2, 
+    6, 7, 2, 
+    5, 7, 6, 
+    6, 4, 5, 
+	6, 2, 0, 
+	0, 4, 6, 
+	5, 1, 0, 
+	0, 4, 5 ]);
+
+  var o = new Object(); // Utilize Object object to return multiple buffer objects together
+
+  // Write vertex information to buffer object
+  o.vertexBuffer = initArrayBufferForLaterUse(gl, vertices, 3, gl.FLOAT);
+  o.colorBuffer = initArrayBufferForLaterUse(gl, colors, 3, gl.FLOAT);
+  o.indexBuffer = initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE);
+  if (!o.vertexBuffer || !o.colorBuffer || !o.indexBuffer) return null; 
+
+  o.numIndices = indices.length;
+
+  // Unbind the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  return o;
+}
 function initVertexBuffersForTriangle(gl) {
   // Create a triangle
   //       v2
@@ -774,7 +875,7 @@ function squareThree()
 {
 	Sqr3Y -= .1;
 	
-	if(Sqr3Y <= -7)
+	if(Sqr3Y <= -8)
 	{
 		Sqr3Y = 7;
 		Sqr3X = getRandomInt() * -5;	
@@ -786,7 +887,7 @@ function squareFour()
 {
 	Sqr4Y -= .1;
 	
-	if(Sqr4Y <= -7)
+	if(Sqr4Y <= -8)
 	{
 		Sqr4Y = 7;
 		Sqr4X = getRandomInt() * 5;	
@@ -794,40 +895,83 @@ function squareFour()
 	
 }
 
+function squareFive(){
+	Sqr5Z += .1;
+	if(Sqr5Z >= 11)
+	{
+		Sqr5Z = -7;
+		Sqr5X = (getRandomInt() * 5 - 5);	
+		Sqr5Y = (getRandomInt() * 5 - 5);	
+	}
+}
+function squareSix(){
+	Sqr6Z += .1;
+	if(Sqr6Z >= 11)
+	{
+		Sqr6Z = -7;
+		Sqr6X = (getRandomInt() * 5 - 5);	
+		Sqr6Y = (getRandomInt() * 5 - 5);	
+	}
+}
 function handleKeyUp(event) {
     currentlyPressedKeys[event.keyCode] = false;
 }
 
-function restart() {
-	location.reload(true);
+document.getElementById("restart_button").onclick = function() {
+	location.reload(true); // refreshes page
+};
+
+function gameOver(){
+	play = false;
+	document.getElementById("pause_screen").style.display = "block";
+	document.getElementById("display").innerHTML = "You lose! Score: " + timer;
+	document.getElementById("restart_button").style.display = "block";
 }
 
 function winLossCheck()
 {
-	
-	if((CharR > Sqr1XL && CharR < Sqr1XR && CharT > Sqr1XB && CharB < Sqr1XT) || (CharL > Sqr1XR && CharL < Sqr1XL &&  CharT > Sqr1XB && CharB < Sqr1XT)
+	if(timer >= 1800){
+		//You win! Amazing
+		play = false;
+		document.getElementById("pause_screen").style.display = "block";
+		document.getElementById("display").innerHTML = "You win! Score: " + timer;
+		document.getElementById("restart_button").style.display = "block";
+	}
+	// Lose conditions / collision detection:
+	else if((CharR > Sqr1XL && CharR < Sqr1XR && CharT > Sqr1XB && CharB < Sqr1XT) || (CharL > Sqr1XR && CharL < Sqr1XL &&  CharT > Sqr1XB && CharB < Sqr1XT)
 	||(CharT > Sqr1XB && CharT < Sqr1XT && CharR > Sqr1XL && CharL < Sqr1XR) || (CharB < Sqr1XT && CharB > Sqr1XB && CharR > Sqr1XL && CharL < Sqr1XR))
 	{
-		restart();
+		gameOver();
 	}
 	
-	if((CharR > Sqr2XL && CharR < Sqr2XR && CharT > Sqr2XB && CharB < Sqr2XT) || (CharL > Sqr2XR && CharL < Sqr2XL &&  CharT > Sqr2XB && CharB < Sqr2XT)
+	else if((CharR > Sqr2XL && CharR < Sqr2XR && CharT > Sqr2XB && CharB < Sqr2XT) || (CharL > Sqr2XR && CharL < Sqr2XL &&  CharT > Sqr2XB && CharB < Sqr2XT)
 	||(CharT > Sqr2XB && CharT < Sqr2XT && CharR > Sqr2XL && CharL < Sqr2XR) || (CharB < Sqr2XT && CharB > Sqr2XB && CharR > Sqr2XL && CharL < Sqr2XR))
 	{
-		restart();
+		gameOver();
 	}
 	
-	if((CharR > Sqr3XL && CharR < Sqr3XR && CharT > Sqr3XB && CharB < Sqr3XT) || (CharL > Sqr3XR && CharL < Sqr3XL &&  CharT > Sqr3XB && CharB < Sqr3XT)
+	else if((CharR > Sqr3XL && CharR < Sqr3XR && CharT > Sqr3XB && CharB < Sqr3XT) || (CharL > Sqr3XR && CharL < Sqr3XL &&  CharT > Sqr3XB && CharB < Sqr3XT)
 	||(CharT > Sqr3XB && CharT < Sqr3XT && CharR > Sqr3XL && CharL < Sqr3XR) || (CharB < Sqr3XT && CharB > Sqr3XB && CharR > Sqr3XL && CharL < Sqr3XR))
 	{
-		restart();
+		gameOver();
 	}
 	
-	if((CharR > Sqr4XL && CharR < Sqr4XR && CharT > Sqr4XB && CharB < Sqr4XT) || (CharL > Sqr4XR && CharL < Sqr4XL &&  CharT > Sqr4XB && CharB < Sqr4XT)
+	else if((CharR > Sqr4XL && CharR < Sqr4XR && CharT > Sqr4XB && CharB < Sqr4XT) || (CharL > Sqr4XR && CharL < Sqr4XL &&  CharT > Sqr4XB && CharB < Sqr4XT)
 	||(CharT > Sqr4XB && CharT < Sqr4XT && CharR > Sqr4XL && CharL < Sqr4XR) || (CharB < Sqr4XT && CharB > Sqr4XB && CharR > Sqr4XL && CharL < Sqr4XR))
 	{
-		restart();
+		gameOver();
 	}
+	else if((Sqr5Z>-0.25 && Sqr5Z < 0.25)&&((CharR > Sqr5XL && CharR < Sqr5XR && CharT > Sqr5XB && CharB < Sqr5XT) || (CharL > Sqr5XR && CharL < Sqr5XL &&  CharT > Sqr5XB && CharB < Sqr5XT)
+	||(CharT > Sqr5XB && CharT < Sqr5XT && CharR > Sqr5XL && CharL < Sqr5XR) || (CharB < Sqr5XT && CharB > Sqr5XB && CharR > Sqr5XL && CharL < Sqr5XR)))
+	{
+		gameOver();
+	}
+	else if((Sqr6Z>-0.25 && Sqr6Z < 0.25)&&((CharR > Sqr6XL && CharR < Sqr6XR && CharT > Sqr6XB && CharB < Sqr6XT) || (CharL > Sqr6XR && CharL < Sqr6XL &&  CharT > Sqr6XB && CharB < Sqr6XT)
+	||(CharT > Sqr6XB && CharT < Sqr6XT && CharR > Sqr6XL && CharL < Sqr6XR) || (CharB < Sqr6XT && CharB > Sqr6XB && CharR > Sqr6XL && CharL < Sqr6XR)))
+	{
+		gameOver();
+	}
+	
 	
 }
 
